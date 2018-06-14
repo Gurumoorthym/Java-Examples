fbe0affdc598f7b6b1fe45ee6b59be4040f94244
vedanth <vedanth@192.168.2.3>
2018-06-14 21:25:03 +0530

    Comments added

--- src/fancyHashMapExample/LinkedFancyHashMap.java
+++ src/fancyHashMapExample/LinkedFancyHashMap.java
@@ -24,18 +24,25 @@ public class LinkedFancyHashMap<E> extends LinkedList<E> {
 		this.fancyHash = fancyHash;
 	}
 	
+	/*
+	 * Addind the hashmap so we can have faster retival
+	 * @see java.util.LinkedList#addLast(java.lang.Object)
+	 */
 	@Override
 	public void addLast(E e) {
 		super.addLast(e);
 		hash.put(this.indexOf(e), e);
     }
-	
+	/*
+	 * Method uses hashmap fetch to get the value rather iterating through the list
+	 * @see java.util.LinkedList#get(int)
+	 */
 	@Override
 	public E get(int index) {
 		return hash.get(index);
 	}    
 	
-	
+	//Remove method to remove from the actual map rather than the list
 	public void remove(String key) {
 		ListIterator<E> it = this.listIterator();
 		while(it.hasNext()) {
@@ -46,7 +53,8 @@ public class LinkedFancyHashMap<E> extends LinkedList<E> {
 			}
 		}
     }
-	
+
+	//Reset the hashMap with respect to the indexing
 	private void resetHashMap() {
 		hash = new HashMap<Integer, E>();
 		for(E e : this) {
